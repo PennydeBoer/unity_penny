fd178bb8cadad9798a4943778f770e1f2b58826c
Penny <38731@ma-web.nl>
2024-10-22 15:31:26 +0200

    movements

---
+++ HelloWorld/Assets/MovementScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * 100);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * 5);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //rb.MoveRotation(Quaternion.Euler(0, 3, 0));
+            rb.rotation = (Quaternion.Euler(0, 1+ rb.rotation.eulerAngles.y, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rb.AddForce(transform.forward * -100);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(transform.forward * -5);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //rb.MoveRotation(Quaternion.Euler(0, 3, 0));
+            rb.rotation = (Quaternion.Euler(0, -1 + rb.rotation.eulerAngles.y, 0));
+        }
+    }
+}
---
+++ HelloWorld/readme.md
@@ -0,0 +1,8 @@
+#Oplossingen
+ * ik heb in c# een code gemaakt waarmee je naar voren en achter kan bewegen
+ * ik heb daar ook een rotatie code toegevoegd voor links en rechts
+ * ook heb ik invisible walls toegevoegd en de gebouwen colliders gegeven
+
+#volgende keer
+ * volgende keer ga ik meer moderne code voor beweging
+ * meer oplossingen zoeken voordat ik hulp vraag
